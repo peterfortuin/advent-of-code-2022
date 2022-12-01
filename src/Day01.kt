e48b2adbec05e1e3758100a74fa9ec0b67b54dd0
Peter Fortuin <peter.fortuin@gmail.com>
2022-12-01 20:48:58 +0100

    Puzzle 01.1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,21 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val elfs = input.asElfs()
+        val caloriesPerElf = elfs.map { elfCaloriesList ->
+            elfCaloriesList.map { calories ->
+                calories.toInt()
+            }.toIntArray().sum()
+        }
+
+        val highestCaloryElf = caloriesPerElf.foldIndexed(Pair<Int, Int>(0, 0)) { index, acc, elfCalories ->
+            if (elfCalories > acc.second) {
+                Pair(index, elfCalories)
+            } else {
+                acc
+            }
+        }
+
+        return highestCaloryElf.second
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +24,23 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+fun List<String>.asElfs(): List<List<String>> {
+    val elfList = mutableListOf<MutableList<String>>()
+    elfList.add(mutableListOf())
+
+    return this.fold(elfList) { acc, line ->
+        if (line.isBlank()) {
+            elfList.add(mutableListOf())
+        } else {
+            elfList.last().add(line)
+        }
+        acc
+    }
 }
