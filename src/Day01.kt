b4275ffb7147b1c7306d07e5171bf6674953647b
Peter Fortuin <peter.fortuin@gmail.com>
2022-12-01 21:24:43 +0100

    Puzzle 01.2

--- src/Day01.kt
+++ src/Day01.kt
@@ -19,12 +19,23 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elfs = input.asElfs()
+        val caloriesPerElf = elfs.map { elfCaloriesList ->
+            elfCaloriesList.map { calories ->
+                calories.toInt()
+            }.toIntArray().sum()
+        }
+
+        val top3Elfs = caloriesPerElf.sortedDescending().subList(0, 3)
+        val totalOfTop3 = top3Elfs.sum()
+
+        return totalOfTop3
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
