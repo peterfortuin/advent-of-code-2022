f92c8d8a249c444fd01c4dffc6c9dd6b8d55401c
Peter Fortuin <peter.fortuin@gmail.com>
2022-12-02 13:22:57 +0100

    Puzzle 02.2

--- src/Day02.kt
+++ src/Day02.kt
@@ -20,12 +20,29 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { rps ->
+            when (rps) {
+                "A X" -> 0 + 3// Rock lose scissors
+                "A Y" -> 3 + 1// Rock draw rock
+                "A Z" -> 6 + 2// Rock win paper
+
+                "B X" -> 0 + 1// Paper lose rock
+                "B Y" -> 3 + 2// Paper draw paper
+                "B Z" -> 6 + 3// Paper win scissors
+
+                "C X" -> 0 + 2// Scissors lose paper
+                "C Y" -> 3 + 3// Scissors draw scissors
+                "C Z" -> 6 + 1// Scissors win rock
+
+                else -> 0
+            }
+        }.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 15)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day02")
     println("Part 1 = ${part1(input)}")
