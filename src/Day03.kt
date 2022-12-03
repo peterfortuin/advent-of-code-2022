016ed9566d13409f522146bef2b979b40d7948dd
Peter Fortuin <peter.fortuin@gmail.com>
2022-12-03 19:57:19 +0100

    Puzzle 03.1

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,6 +1,23 @@
 fun main() {
+    fun findItemInBothLists(first: List<Int>, second: List<Int>): Int {
+        val firstSet = first.toSet()
+        val secondSet = second.toSet()
+        return firstSet.intersect(secondSet).first()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val incorrectSupplies = input.map { line ->
+            val l = line.toCharArray().map {
+                when (it.code) {
+                    in 97..122 -> it.code - 96
+                    in 65..90 -> it.code - 38
+                    else -> 0
+                }
+            }
+            val twoBags = Pair(l.subList(0, l.size / 2), l.subList(l.size / 2, l.size))
+            findItemInBothLists(twoBags.first, twoBags.second)
+        }
+        return incorrectSupplies.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +26,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 157)
 
     val input = readInput("Day03")
     println("Part 1 = ${part1(input)}")
