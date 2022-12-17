dd2e0ef8446a769ec3e20238025287fa721402e1
Peter Fortuin <peter.fortuin@gmail.com>
2022-12-17 10:52:17 +0100

    Puzzle 03.2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,18 +1,8 @@
 fun main() {
-    fun findItemInBothLists(first: List<Int>, second: List<Int>): Int {
-        val firstSet = first.toSet()
-        val secondSet = second.toSet()
-        return firstSet.intersect(secondSet).first()
-    }
-
     fun part1(input: List<String>): Int {
         val incorrectSupplies = input.map { line ->
             val l = line.toCharArray().map {
-                when (it.code) {
-                    in 97..122 -> it.code - 96
-                    in 65..90 -> it.code - 38
-                    else -> 0
-                }
+                charToNumber(it)
             }
             val twoBags = Pair(l.subList(0, l.size / 2), l.subList(l.size / 2, l.size))
             findItemInBothLists(twoBags.first, twoBags.second)
@@ -21,14 +11,48 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.devideInGroupsOf3Lines()
+            .map { group ->
+                group.findUniqueItemInAllLines()
+            }.sumOf { item -> charToNumber(item) }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println("Part 1 = ${part1(input)}")
     println("Part 2 = ${part2(input)}")
+}
+
+fun findItemInBothLists(first: List<Int>, second: List<Int>): Int {
+    val firstSet = first.toSet()
+    val secondSet = second.toSet()
+    return firstSet.intersect(secondSet).first()
+}
+
+fun charToNumber(it: Char) = when (it.code) {
+    in 97..122 -> it.code - 96
+    in 65..90 -> it.code - 38
+    else -> 0
+}
+
+private fun List<String>.findUniqueItemInAllLines(): Char {
+    val firstSet = this[0].toSet()
+    val secondSet = this[1].toSet()
+    val thirdSet = this[2].toSet()
+    val intersect = firstSet.intersect(secondSet).intersect(thirdSet)
+    return intersect.first()
+}
+
+private fun <E> List<E>.devideInGroupsOf3Lines(): List<List<E>> {
+    val result = mutableListOf<List<E>>()
+    var i = 0
+    while (i < this.size) {
+        result.add(this.subList(i, i + 3))
+        i += 3
+    }
+    return result
 }
