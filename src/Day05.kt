72224ad26695a965739c259b41943bb194ceabdb
Peter Fortuin <peter.fortuin@gmail.com>
2022-12-18 18:32:58 +0100

    Puzzle 05.1

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,6 +1,63 @@
+import java.util.*
+
+data class MoveInstruction(val amount: Int, val from: Int, val to: Int)
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun parseInput(input: List<String>): Pair<List<Stack<Char>>, List<MoveInstruction>> {
+        val stacks = listOf(
+            Stack<Char>(),
+            Stack<Char>(),
+            Stack<Char>(),
+            Stack<Char>(),
+            Stack<Char>(),
+            Stack<Char>(),
+            Stack<Char>(),
+            Stack<Char>(),
+            Stack<Char>()
+        )
+        val moveInstructions = mutableListOf<MoveInstruction>()
+
+        input.forEach { line ->
+            when {
+                line.contains("[") -> {
+                    if (line.isNotEmpty() && line[1] != ' ') stacks[0].push(line[1])
+                    if (line.length > 3 && line[5] != ' ') stacks[1].push(line[5])
+                    if (line.length > 7 && line[9] != ' ') stacks[2].push(line[9])
+                    if (line.length > 11 && line[13] != ' ') stacks[3].push(line[13])
+                    if (line.length > 15 && line[17] != ' ') stacks[4].push(line[17])
+                    if (line.length > 19 && line[21] != ' ') stacks[5].push(line[21])
+                    if (line.length > 23 && line[25] != ' ') stacks[6].push(line[25])
+                    if (line.length > 27 && line[29] != ' ') stacks[7].push(line[29])
+                    if (line.length > 31 && line[33] != ' ') stacks[8].push(line[33])
+                }
+
+                line.startsWith("move") -> {
+                    val parts = line.split(" ")
+                    val amount = parts[1].toInt()
+                    val from = parts[3].toInt()
+                    val to = parts[5].toInt()
+                    moveInstructions.add(MoveInstruction(amount, from, to))
+                }
+            }
+        }
+
+        stacks.map { it.reverse() }
+
+        return Pair(stacks, moveInstructions)
+    }
+
+    fun part1(input: List<String>): String {
+        val (stacks, moveInstructions) = parseInput(input)
+
+        moveInstructions.forEach { move ->
+            val fromStack = stacks[move.from - 1]
+            val toStack = stacks[move.to - 1]
+            repeat(move.amount) {
+                toStack.push(fromStack.pop())
+            }
+        }
+
+        return stacks.map<Stack<Char>, Any> { it.lastOrNull() ?: "" }.joinToString("")
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +66,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == "CMZ")
 
     val input = readInput("Day05")
     println("Part 1 = ${part1(input)}")
