7cdd4a60810dfac069d28afb8b38cb8ed561569c
Peter Fortuin <peter.fortuin@gmail.com>
2022-12-19 09:10:50 +0100

    Puzzle 06.1

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,6 +1,15 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputLine = input.first()
+        var index = 4
+        inputLine.toCharArray().toList().windowed(4).forEach { possibleStartOfPacketMarker ->
+            val pSOPMList = possibleStartOfPacketMarker.toCharArray().toList()
+            if (pSOPMList.distinct() == pSOPMList) {
+                return index
+            }
+            index++
+        }
+        return -1
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +18,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day06_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 5)
 
     val input = readInput("Day06")
     println("Part 1 = ${part1(input)}")
