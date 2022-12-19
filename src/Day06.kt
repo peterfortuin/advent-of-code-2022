9dbaa274ef86e12c726985ffb4192cf639b15913
Peter Fortuin <peter.fortuin@gmail.com>
2022-12-19 09:21:56 +0100

    Puzzle 06.2

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,8 +1,7 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        val inputLine = input.first()
-        var index = 4
-        inputLine.toCharArray().toList().windowed(4).forEach { possibleStartOfPacketMarker ->
+    fun uniqueValuesFinder(input: String, length: Int): Int {
+        var index = length
+        input.toCharArray().toList().windowed(length).forEach { possibleStartOfPacketMarker ->
             val pSOPMList = possibleStartOfPacketMarker.toCharArray().toList()
             if (pSOPMList.distinct() == pSOPMList) {
                 return index
@@ -12,15 +11,27 @@ fun main() {
         return -1
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: String): Int {
+        return uniqueValuesFinder(input, 4)
+    }
+
+    fun part2(input: String): Int {
+        return uniqueValuesFinder(input, 14)
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day06_test")
-    check(part1(testInput) == 5)
+    check(part1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5)
+    check(part1("nppdvjthqldpwncqszvftbrmjlhg") == 6)
+    check(part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10)
+    check(part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11)
 
-    val input = readInput("Day06")
+    check(part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb") == 19)
+    check(part2("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23)
+    check(part2("nppdvjthqldpwncqszvftbrmjlhg") == 23)
+    check(part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29)
+    check(part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26)
+
+    val input = readInput("Day06").first()
     println("Part 1 = ${part1(input)}")
     println("Part 2 = ${part2(input)}")
 }
